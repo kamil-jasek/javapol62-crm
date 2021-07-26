b71838da3c57d73c0a1d44cf699dd9497fec504f
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-07-26 18:48:05 +0200

    added address entity

---
+++ src/main/java/pl/sda/crm/entity/Address.java
@@ -0,0 +1,33 @@
+package pl.sda.crm.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "addresses")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@EqualsAndHashCode
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String street;
+    private String city;
+    private String zipCode;
+    private String country;
+
+    public Address(String street, String city, String zipCode, String country) {
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
+        this.country = country;
+    }
+}
